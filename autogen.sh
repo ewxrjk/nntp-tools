94bbce9b6a6666dc1290f464840bc299873aa48b
Richard Kettlewell <rjk@terraraq.org.uk>
2013-11-01 15:26:42 +0000

    Use autoreconf

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#! /bin/sh
 #
 # This file is part of rjk-nntp-tools.
 # Copyright (C) 2005, 2010-11 Richard Kettlewell
@@ -22,45 +22,6 @@
 set -e
 
 mkdir -p config.aux
-
-# Find an automake
-if [ -z "$AUTOMAKE" ]; then
-  for prog in automake automake-1.11 automake-1.10 automake-1.9 automake-1.8 automake-1.7; do
-    if type $prog >/dev/null 2>&1; then
-      AUTOMAKE=$prog
-      break
-    fi
-  done
-  if [ -s "$AUTOMAKE" ]; then
-    echo "ERROR: no automake found" >&2
-    exit 1
-  fi
-fi
-ACLOCAL=${AUTOMAKE/automake/aclocal}
-
-# Reject ancient versions of Automake
-amv=`${AUTOMAKE} --version|grep ^automake`
-case "$amv" in
-*11.* )
-  ;;
-*1.4 | *1.4.* | *1.5 | *1.5.* | *1.6 | *1.6.* )
-  echo Need Automake 1.7 or better >&2
-  exit 1
-  ;;
-esac
-
 srcdir=$(dirname $0)
-here=$(pwd)
 cd $srcdir
-if [ -d $HOME/usr/share/aclocal ]; then
-  ${ACLOCAL} -I $HOME/usr/share/aclocal -I aclocal
-elif [ -d aclocal ]; then
-  ${ACLOCAL} -I aclocal
-else
-  ${ACLOCAL}
-fi
-autoconf
-autoheader
-${AUTOMAKE} -a || true		# for INSTALL
-${AUTOMAKE} --foreign -a
-rm -f config.cache
+autoreconf -si
