a0eaad848eda9775875906f5b3911f5f26be7be9
Richard Kettlewell <rjk@greenend.org.uk>
2015-06-06 13:51:42 +0100

    news-sources: rudimentary graphs

--- sources/sources.cc
+++ sources/sources.cc
@@ -18,12 +18,17 @@
  * USA
  */
 #include <config.h>
+#include <algorithm>
+#include <cairomm/cairomm.h>
 #include <cerrno>
 #include <cstdint>
 #include <cstdio>
 #include <cstring>
+#include <dirent.h>
 #include <fcntl.h>
 #include <getopt.h>
+#include <map>
+#include <set>
 #include <string>
 #include <sys/mman.h>
 #include <sys/stat.h>
@@ -46,13 +51,28 @@ struct map_entry {
 };
 
 #define MAP_SIZE (60 * 24 * sizeof (map_entry))
+#define MAP_EXTENSION ".nsdat"
 
 static struct tm start_time;
 static std::string state = ".";
+static std::string output = ".";
 static std::string current_path;
 static struct map_entry *current_map;
 static struct timeval latest_time;
 static uint64_t time_counter;
+static std::set<std::string> days_changed;
+static double width = 60 * 24 / 2;
+static double height = 256;
+
+static const double colors[][3]  = {
+  { 1.0, 0.0, 0.0 },
+  { 0.0, 0.0, 1.0 },
+  { 0.0, 1.0, 0.0 },
+  { 1.0, 0.0, 1.0 },
+  { 1.0, 1.0, 0.0 },
+  { 0.0, 1.0, 1.0 },
+  // TODO more; or choose intelligently for the number of peers
+};
 
 class details;
 
@@ -65,15 +85,31 @@ static inline bool operator<(const struct timeval &a,
   else return false;
 }
 
+static inline bool starts_with(const std::string &a,
+                               const std::string &b) {
+  return a.compare(0, b.size(), b) == 0;
+}
+
+static inline bool ends_with(const std::string &a,
+                             const std::string &b) {
+  return (a.size() >= b.size()
+          && a.compare(a.size() - b.size(), b.size(), b) == 0);
+}
+
 static bool process_file(FILE *fp);
 static void process_line(const details &d);
 static void process_accepted(const details &d);
 static void read_timestamp();
 static void update_timestamp();
+static void process_day(const std::string &day);
+static void draw_graph(const std::string &day,
+                       const std::map<std::string,map_entry *> &info);
 
 int main(int argc, char **argv) {
   static const struct option options[] = {
     { "input", required_argument, 0, 'i' },
+    { "state", required_argument, 0, 's' },
+    { "output", required_argument, 0, 'o' },
     { "help", no_argument, 0, 'h' },
     { "version", no_argument, 0, 'V' },
     { 0, 0, 0, 0 }
@@ -82,7 +118,7 @@ int main(int argc, char **argv) {
   int n;
   const char *input = 0;
 
-  while((n = getopt_long(argc, argv, "hVi:s:", options, 0)) >= 0) {
+  while((n = getopt_long(argc, argv, "hVi:s:o:", options, 0)) >= 0) {
     switch(n) {
     case 'h':
       printf("Usage:\n\
@@ -91,6 +127,7 @@ int main(int argc, char **argv) {
 Options:\n\
   -i, --input PATH                  Input file (default: stdin)\n\
   -s, --state DIR                   State directory (default: .)\n\
+  -o, --output DIR                  Ouptut directory (default: .)\n\
   -h, --help                        Display usage message\n\
   -V, --version                     Display version number\n");
       return 0;
@@ -103,19 +140,25 @@ Options:\n\
     case 's':
       state = optarg;
       break;
+    case 'o':
+      output = optarg;
+      break;
     default:
       return 1;
     }
   }
 
+  // Find the current time, for later guessing what year a timestamp refers to
   struct timeval now;
   if(gettimeofday(&now, NULL) == -1)
     fatal(errno, "gettimeofday");
   if(!localtime_r(&now.tv_sec, &start_time))
     fatal(errno, "localtime_r");
 
+  // Read the last-processed timestamp.  Any older records are ignored.
   read_timestamp();
 
+  // Read the input.
   if(input) {
     FILE *fp = fopen(input, "r");
     if(!fp)
@@ -128,11 +171,16 @@ Options:\n\
       fatal(errno, "reading stdin");
   }
 
+  // Commit the last-processed timestamp.
   update_timestamp();
+
+  // Render graphs for every day for which new data was gathered.
+  std::for_each(days_changed.begin(), days_changed.end(), process_day);
 
   return 0;
 }
 
+// General-purpose line parser
 class parser {
 public:
   std::string line;
@@ -202,6 +250,7 @@ public:
   }
 };
 
+// Represent and parse a line from the news log
 class details {
 public:
   struct tm when;
@@ -250,8 +299,9 @@ public:
     if(code == ART_ACCEPT || code == ART_JUNK) {
       if(!p.get_int(size))
         return false;
-    } else
-      extra.assign(p.line, p.pos, std::string::npos);
+    }
+    p.skip_spaces();
+    extra.assign(p.line, p.pos, std::string::npos);
     when.tm_year = start_time.tm_year;
     if(when.tm_mon > start_time.tm_mon)
       --when.tm_year;
@@ -283,36 +333,43 @@ static void process_line(const details &d) {
   }
 }
 
+static map_entry *open_map(const std::string &path) {
+  int fd;
+  if((fd = open(path.c_str(), O_RDWR|O_CREAT, 0666)) < 0)
+    fatal(errno, "open %s", path.c_str());
+  if(ftruncate(fd, MAP_SIZE) < 0)
+    fatal(errno, "ftruncate %s", path.c_str());
+  void *new_map = mmap(0, MAP_SIZE, PROT_READ|PROT_WRITE,
+                       MAP_SHARED, fd, 0);
+  if(new_map == (void *)-1)
+    fatal(errno, "mmap %s", path.c_str());
+  if(close(fd) < 0)
+    fatal(errno, "close %s", path.c_str());
+  return (struct map_entry *)new_map;
+}
+
+static void close_map(struct map_entry *map) {
+  if(map) {
+    if(munmap(current_map, MAP_SIZE) < 0)
+      fatal(errno, "munmap");
+  }
+ }
+
 static void process_accepted(const details &d) {
-  char buffer[32];
-  snprintf(buffer, sizeof buffer, "%04d-%02d-%02d-",
+  char day[32];
+  snprintf(day, sizeof day, "%04d-%02d-%02d",
            d.when.tm_year + 1900, d.when.tm_mon + 1, d.when.tm_mday);
-  std::string path = state + "/" + buffer;
+  std::string path = state + "/" + day + "-";
   for(size_t pos = 0; pos < d.peer.size(); ++pos) {
     if(d.peer.at(pos) > ' ' && d.peer.at(pos) < 0x7F)
       path += d.peer.at(pos);
     else
       path += 'X';
   }
+  path += MAP_EXTENSION;
   if(path != current_path) {
-    if(current_map) {
-      if(munmap(current_map, MAP_SIZE) < 0)
-        fatal(errno, "munmap %s", current_path.c_str());
-    }
-    current_path.clear();
-    current_map = 0;
-    int fd;
-    if((fd = open(path.c_str(), O_RDWR|O_CREAT, 0666)) < 0)
-      fatal(errno, "open %s", path.c_str());
-    if(ftruncate(fd, MAP_SIZE) < 0)
-      fatal(errno, "ftruncate %s", path.c_str());
-    void *new_map = mmap(0, MAP_SIZE, PROT_READ|PROT_WRITE,
-                         MAP_SHARED, fd, 0);
-    if(new_map == (void *)-1)
-      fatal(errno, "mmap %s", path.c_str());
-    current_map = (map_entry *)new_map;
-    if(close(fd) < 0)
-      fatal(errno, "close %s", path.c_str());
+    close_map(current_map);
+    current_map = open_map(path);
     current_path = path;
   }
   current_map[60 * d.when.tm_hour + d.when.tm_min].articles += 1;
@@ -324,6 +381,7 @@ static void process_accepted(const details &d) {
   ++time_counter;
   if(time_counter >= 32)
     update_timestamp();
+  days_changed.insert(day);
 }
 
 static void read_timestamp() {
@@ -351,4 +409,96 @@ static void update_timestamp() {
      || fclose(fp) < 0)
     fatal(errno, "writing timestamp");
   time_counter = 0;
+}
+
+static void process_day(const std::string &day) {
+  std::map<std::string,map_entry *> info;
+  DIR *dp;
+  struct dirent *de;
+  if(!(dp = opendir(state.c_str())))
+    fatal(errno, "opendir %s", state.c_str());
+  while((de = readdir(dp))) {
+    std::string name = de->d_name;
+    if(starts_with(name, day)
+       && name[day.size()] == '-'
+       && ends_with(name, MAP_EXTENSION)) {
+      std::string path = state + "/" + name;
+      std::string peer(name, day.size() + 1,
+                       name.size() - (1 + day.size() + strlen(MAP_EXTENSION)));
+      info[peer] = open_map(path);
+    }
+  }
+  closedir(dp);
+  draw_graph(day, info);
+  std::for_each(info.begin(), info.end(),
+                [] (const std::pair<std::string,map_entry *> &m) {
+                  close_map(m.second);
+                });
+}
+
+static void draw_graph(const std::string &day,
+                       const std::map<std::string,map_entry *> &info) {
+  // Find the maximum size
+  double max_articles = 0;
+  double max_bytes = 0;
+  for(int n = 0; n < 24 * 60; ++n) {
+    double articles = 0, bytes = 0;
+    std::for_each(info.begin(), info.end(),
+                  [&] (const std::pair<std::string,map_entry *> &m) {
+                    articles += m.second[n].articles;
+                    bytes += m.second[n].bytes;
+                  });
+    max_articles = std::max(articles, max_articles);
+    max_bytes = std::max(bytes, max_bytes);
+  }
+
+  Cairo::RefPtr<Cairo::Surface> surface_articles
+    = Cairo::ImageSurface::create(Cairo::FORMAT_ARGB32, width, height);
+  Cairo::RefPtr<Cairo::Context> context_articles
+    = Cairo::Context::create(surface_articles);
+  Cairo::RefPtr<Cairo::Surface> surface_bytes
+    = Cairo::ImageSurface::create(Cairo::FORMAT_ARGB32, width, height);
+  Cairo::RefPtr<Cairo::Context> context_bytes
+    = Cairo::Context::create(surface_bytes);
+  context_articles->set_source_rgb(1.0, 1.0, 1.0);
+  context_articles->paint();
+  context_bytes->set_source_rgb(1.0, 1.0, 1.0);
+  context_bytes->paint();
+  for(int n = 0; n < 24 * 60; ++n) {
+    uint64_t articles_below = 0;
+    uint64_t bytes_below = 0;
+    int npeer = 0;
+    std::for_each(info.begin(), info.end(),
+                  [&] (const std::pair<std::string,map_entry *> &m) {
+                    uint64_t articles = m.second[n].articles;
+                    uint64_t bytes = m.second[n].bytes;
+                    if(bytes) {
+                      context_bytes->set_source_rgb(colors[npeer][0],
+                                                       colors[npeer][1],
+                                                       colors[npeer][2]);
+                      double y0 = bytes_below * height / max_bytes;
+                      double y1 = (bytes_below + bytes) * height / max_bytes;
+                      double x0 = n * width / (24 * 60);
+                      double x1 = (n + 1) * width / (24 * 60);
+                      context_bytes->rectangle(x0, height - y1, x1 - x0, y1 - y0);
+                      context_bytes->fill();
+                      bytes_below += bytes;
+                    }
+                    if(articles) {
+                      context_articles->set_source_rgb(colors[npeer][0],
+                                                       colors[npeer][1],
+                                                       colors[npeer][2]);
+                      double y0 = articles_below * height / max_articles;
+                      double y1 = (articles_below + articles) * height / max_articles;
+                      double x0 = n * width / (24 * 60);
+                      double x1 = (n + 1) * width / (24 * 60);
+                      context_articles->rectangle(x0, height - y1, x1 - x0, y1 - y0);
+                      context_articles->fill();
+                      articles_below += articles;
+                    }
+                    ++npeer;
+                  });
+  }
+  surface_articles->write_to_png(output + "/" + day + "-articles.png");
+  surface_bytes->write_to_png(output + "/" + day + "-bytes.png");
 }
