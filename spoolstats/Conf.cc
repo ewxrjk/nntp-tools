92f1ac4a0cea59a22877533bb825cdd2a87b7d82
Richard Kettlewell <rjk@greenend.org.uk>
2015-06-06 12:29:32 +0100

    Enable C++11 builds.

--- spoolstats/Conf.cc
+++ spoolstats/Conf.cc
@@ -171,7 +171,7 @@ void Config::footer(ostream &os) {
      << "</p>\n";
   os << "<p class=credits><a href="
      << HTML::Quote("http://www.greenend.org.uk/rjk/2006/newstools.html")
-     << ">spoolstats "VERSION"</a></p>\n";
+     << ">spoolstats " VERSION "</a></p>\n";
 }
 
 map<string, Hierarchy *> Config::hierarchies;
